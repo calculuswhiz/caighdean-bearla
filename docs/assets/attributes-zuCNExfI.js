9c6cbc6f382308ca56f3bca443594c32af227719
Zachary Bian <calculuswhiz@gmail.com>
2025-10-03 13:14:36 -0400

    Translated Chapter 4 main text

--- docs/assets/attributes-zuCNExfI.js
+++ docs/assets/attributes-zuCNExfI.js
@@ -175,32 +175,32 @@ const a=`:chapterTitle_ga: AN AIDIACHT
 :4-4-1-b-i-p_ga: Cuirtear _-e_ leis an gconsan deiridh, agus caolaítear é más leathan dó, e.g., _gorm_, _goirme_; _maith_, _maithe_ mar a dhéantar le hainmfhocail sa Dara Díochlaonadh (féach 2.3.2).
 :4-4-1-b-i-p_en: _-e_ is put with the final consonant, and it is narrowed if broad, e.g., _gorm_, _goirme_; _maith_, _maithe_ as would be done with nouns in the Second Declension (see 2.3.2).
 
-:4-4-1-b-ii-p_ga: I gcás aidiachtaí a chríochnaíonn ar _-(e)ach_, cuirtear _-(a)í_ in ionad _-(e)ach_, e.g., _isteach_, _aistí_; _iontach_, _iontaí_; ; mar a dhéantar le hainmfhocail sa Dara Díochlaonadh (féach 2.3.2).
-:4-4-1-b-ii-p_en: [TODO] 
+:4-4-1-b-ii-p_ga: I gcás aidiachtaí a chríochnaíonn ar _-(e)ach_, cuirtear _-(a)í_ in ionad _-(e)ach_, e.g., _isteach_, _aistí_; _iontach_, _iontaí_; mar a dhéantar le hainmfhocail sa Dara Díochlaonadh (féach 2.3.2).
+:4-4-1-b-ii-p_en: When adjectives end with _-(e)ach_, _-(a)í_ replaces _-(e)ach_, e.g., _isteach_, _aistí_; _iontach_, _iontaí_; as would be done with nouns in the Second Declension (see 2.3.2).
 
 :4-4-1-b-iii-p_ga: I gcás aidiachtaí a chríochnaíonn ar _-íoch_, athraítear _-íoch_ go _-íche_, e.g., _eacnamaíoch_, _eacnamaíche_.
-:4-4-1-b-iii-p_en: [TODO] 
+:4-4-1-b-iii-p_en: When adjectives end with _-íoch_, _-íoch_ is changed to _-íche_, e.g., _eacnamaíoch_, _eacnamaíche_.
 
 :4-4-1-b-iv-p_ga: Féach Caibidil 2 don chur síos cuimsitheach ar athrú gutaí.
-:4-4-1-b-iv-p_en: [TODO] 
+:4-4-1-b-iv-p_en: See Chapter 2 for a comprehensive description of vowel changes.
 
 :4-4-2-title_ga: Foirm an Ghairmigh Uatha
 :4-4-2-title_en: Form of the Vocative Singular
 
 :4-4-2-p_ga: Is ionann foirm don ghairmeach uatha agus don ainmneach uatha, ach amháin i gcás aidiachtaí a gcaolaítear a gconsan deiridh sa ghinideach uatha firinscneach. I gcás na n-aidiachtaí sin, is gnách gurb ionann foirm an ghairmigh uatha fhirinscnigh agus foirm an ghinidigh uatha fhirinscnigh, e.g., _a fhir mhóir_; _cóta an fhir mhóir_.
-:4-4-2-p_en: [TODO] 
+:4-4-2-p_en: The form of the vocative singular is the same as the nominative singular, except when adjectives whose final consonant is syncopated in the masculine genitive singular. With such adjectives, the form of the masculine vocative singular is usually the same as the form of the masculine genitive singular, e.g., _a fhir mhóir_; _cóta an fhir mhóir_.
 
 :4-4-3-title_ga: Foirm an Iolra -- Gach Tuiseal
 :4-4-3-title_en: Form of the Plural -- Every Case
 
 :4-4-3-p_ga: Cuirtear _-a_ leis na haidiachtaí a chríochnaíonn ar chonsan leathan agus _-e_ leis na haidiachtaí a chríochnaíonn ar chonsan caol agus coimrítear aidiachtaí áirithe (ach féach lagiolraí sa ghinideach iolra, 4.3.2(b)).
-:4-4-3-p_en: [TODO] 
+:4-4-3-p_en: _-a_ is added to adjectives ending in a broad consonant and _-e_ to adjectives ending in a narrow consonant and some adjectives are syncopated (but see weak plurals in the genitive plural, 4.3.2(b)).
 
 :4-4-4-title_ga: Coimriú san Uatha agus san Iolra
 :4-4-4-title_en: Syncopation in the Singular and in the Plural
 
 :4-4-4-p_ga: Déantar coimriú sa tuiseal ginideach uatha baininscneach agus san uimhir iolra i ngach tuiseal ar na haidiachtaí sa tábla seo a leanas, i measc eile, ach ní choimrítear na foirmeacha idir lúibíní.
-:4-4-4-p_en: [TODO] 
+:4-4-4-p_en: The adjectives in the following table, among others, are syncopated in the feminine genitive singular case and in the plural in each case, but the forms in brackets are not syncopated.
 
 :nopass-Table4C-caption_ga: {4-4-4-title}
 :nopass-Table4C-caption_en: {4-4-4-title}
@@ -218,7 +218,7 @@ const a=`:chapterTitle_ga: AN AIDIACHT
 :4-5-title_en: The Second Declension
 
 :4-5-p_ga: Cuimsítear sa díochlaonadh seo aidiachtaí a chríochnaíonn ar _-úil_, mar aon le roinnt aidiachtaí a chríochnaíonn ar _-ir_, e.g., _óir_, _deacair_, _socair_.
-:4-5-p_en: [TODO] 
+:4-5-p_en: This declension includes adjectives that end in _-úil_, as well as some adjectives that end in _-ir_, e.g., _óir_, _díacair_, _socair_.
 
 :4-5-1-title_ga: Foirm an Ghinidigh Uatha
 :4-5-1-title_en: Form of the Genitive Singular
@@ -227,10 +227,10 @@ const a=`:chapterTitle_ga: AN AIDIACHT
 :nopass-4-5-1-a-title_en: {Masculine}
 
 :4-5-1-a-p_ga: Ní théann aon infhilleadh ar aidiachtaí an díochlaonta seo.
-:4-5-1-a-p_en: [TODO] 
+:4-5-1-a-p_en: Adjectives of this declension do not undergo any inflection.
 
 :4-5-1-b-p_ga: Leathnaítear an consan deiridh agus cuirtear _-a_ leis.
-:4-5-1-b-p_en: [TODO] 
+:4-5-1-b-p_en: The final consonant is broadened and _-a_ is added.
 
 :nopass-4-5-1-b-title_ga: {Feminine}
 :nopass-4-5-1-b-title_en: {Feminine}
@@ -239,19 +239,19 @@ const a=`:chapterTitle_ga: AN AIDIACHT
 :4-5-2-title_en: Form of the Plural -- Every Case
 
 :4-5-2-a-p_ga: Leathnaítear an consan deiridh agus cuirtear _-a_ leis mar a dhéantar sa ghinideach uatha baininscneach (ach féach lagiolraí sa ghinideach iolra, 4.3.2(b)).
-:4-5-2-a-p_en: [TODO] 
+:4-5-2-a-p_en: The final consonant is broadened and _-a_ is added as in the feminine genitive singular (but see genitive plurals, 4.3.2(b)).
 
 :4-5-2-b-p_ga: Coimrítear _deacair_, _deacra_; _socair_, _socra_ sa ghinideach uatha agus san uimhir iolra.
-:4-5-2-b-p_en: [TODO] 
+:4-5-2-b-p_en: _deacair_, _deacra_; _socair_, _socra_ are syncopated in the genitive singular and in the plural number.
 
 :4-6-title_ga: An Tríú Díochlaonadh
 :4-6-title_en: The Third Declension
 
 :4-6-1-p_ga: Cuimsítear sa díochlaonadh seo aidiachtaí a chríochnaíonn ar ghuta, e.g., _cliste_, _cróga_.
-:4-6-1-p_en: [TODO] 
+:4-6-1-p_en: This declension includes adjectives that end in a vowel, e.g., _cliste_, _cróga_.
 
 :4-6-2-p_ga: Ní dhéantar infhilleadh ach ar dhá aidiacht sa díochlaonadh seo: _breá_ agus _te_. Athraíonn _breá_ go _breátha_ sa ghinideach uatha baininscneach agus san uimhir iolra agus athraíonn _te_ go _teo_ san uimhir iolra (ach féach lagiolraí sa ghinideach iolra, 4.3.2(b)).
-:4-6-2-p_en: [TODO] 
+:4-6-2-p_en: Only two adjectives are inflected in this declension: _breá_ and _te_. _breá_ changes to _breátha_ in the feminine genitive singular and plural number and _te_ changes to _teo_ in the plural number (but see weak plurals in the genitive plural, 4.3.2(b)).
 
 :4-6-2-sample-row1_ga: Ainmneach uatha
 :4-6-2-sample-row1_en: Nominative singular
@@ -319,61 +319,61 @@ const a=`:chapterTitle_ga: AN AIDIACHT
 :Table4G-caption_en: Summary of the Adjectives' Declensions -- Feminine Plural
 
 :4-8-title_ga: Céimeanna Comparáide na hAidiachta
-:4-8-title_en: [TODO] 
+:4-8-title_en: Degrees of Comparison of the Adjective
 
 :4-8-1-a-p_ga: Is gnách gurb ionann foirm do bhreischéim agus do shárchéim aidiachta agus don ghinideach uatha baininscneach, e.g., _ard_, _airde_; _breá_, _breátha_; _cóir_, _córa_; _dathúil_, _dathúla_; _deacair_, _deacra_; _díreach_, _dírí_.
-:4-8-1-a-p_en: [TODO] 
+:4-8-1-a-p_en: The form of the comparative degrees and superlative forms of adjectives is usually the same as the feminine genitive singular, e.g., _ard_, _airde_; _breá_, _breátha_; _cóir_, _córa_; _dathúil_, _dathúla_; _diacair_, _diacra_; _diríct_, _diríct_.
 
 :4-8-1-b-p_ga: Is ionann freisin foirm do bhreischéim agus do shárchéim aidiachta agus don ghinideach uatha baininscneach i gcás aidiachtaí a chríochnaíonn ar _-íoch_, e.g., _buíoch_, _buíche_; _débhríoch_, _débhríche_; _imníoch_, _imníche_; _impíoch_, _impíche_; _taithíoch_, _taithíche_; _uathoibríoch_, _uathoibríche_.
-:4-8-1-b-p_en: [TODO] 
+:4-8-1-b-p_en: The form for the comparative degrees and superlative forms of adjectives is also the same as for the feminine genitive singular for adjectives ending in _-íoch_, e.g., _buíoch_, _buíche_; _débhríoch_, _débhríche_; _imníoch_, _imníche_; _impíoch_, _impíche_; _taithíoch_, _taithíche_; _uathoibríoch_, _uathoibríche_.
 
 :4-8-2-p_ga: Tá foirmeacha neamhrialta ar roinnt aidiachtaí, e.g., _eag_, _lú_; _dócha_, _dóichí_; _fada_, _faide (sia)_; _fogas_, _foisce_; _furasta_, _fusa_; _gearr_, _giorra_; _iomaí_, _lia_; _maith_, _fearr_; _mór_, _mó_; _olc_, _measa_; _te_, _teo_.
-:4-8-2-p_en: [TODO] 
+:4-8-2-p_en: Some adjectives have irregular forms, e.g., _eag_, _lú_; _dócha_, _dóichí_; _fada_, _faide (sia)_; _fogas_, _foisce_; _furasta_, _fusa_; _gearr_, _giorra_; _iomaí_, _lia_; _maith_, _fearr_; _mór_, _mó_; _olc_, _measa_; _te_, _teo_.
 
 :4-8-3-title_ga: Foirmeacha ar lorg _dá_ agus _a_
-:4-8-3-title_en: [TODO] 
+:4-8-3-title_en: Forms following _dá_ and _a_
 
-:4-8-3-a-p_ga: I ndiaidh _dá_ agus _a_ , is cuma feidhm réamhthagrach nó feidhm iarthagrach a bheith acu, is gnách go n-úsáidtear foirm is ionann agus foirm na breischéime den aidiacht nó foirm a dhíorthaíonn ón mbreischéim nó ón mbunchéim agus a chríochnaíonn ar _cht_.
-:4-8-3-a-p_en: [TODO] 
+:4-8-3-a-p_ga: I ndiaidh _dá_ agus _a_, is cuma feidhm réamhthagrach nó feidhm iarthagrach a bheith acu, is gnách go n-úsáidtear foirm is ionann agus foirm na breischéime den aidiacht nó foirm a dhíorthaíonn ón mbreischéim nó ón mbunchéim agus a chríochnaíonn ar _-cht_.
+:4-8-3-a-p_en: After _dá_ and _a_, regardless of whether they have a proleptic or analeptic(?) function, a form that is the same as the comparative form of the adjective or a form that derives from the comparative or the base form and ends in _-cht_ is usually used.
 
 :4-8-3-b-p_ga: Ní úsáidtear ach foirm amháin i gcás na n-aidiachtaí seo a leanas: _dócha_, _dóichí_; _dona_, _donacht_; _furasta_, _fusacht_; _gearr_, _giorracht_; agus i gcás na leaganacha neamhrialta seo: _beag_, _laghad_; _fada_, _fad_; _iomaí_, _liacht_; _maith_, _feabhas_; _mór_, _méad/méid_; _olc_, _olcas_; _tiubh_, _tiús_.
-:4-8-3-b-p_en: [TODO] 
+:4-8-3-b-p_en: Only one form is used for the following adjectives: _dócha_, _dóichí_; _dona_, _donacht_; _furasta_, _fusacht_; _gearr_, _giorracht_; and for these irregular forms: _beag_, _laghad_; _fada_, _fad_; _iomaí_, _liacht_; _maith_, _feabhas_; _mór_, _méad/méid_; _olc_, _olcas_; _tiubh_, _tiús_. (_Translation note_: These are abstract nouns. E.g. _beag_ (_small_) → _laghad_ (_smallness_))
 
 :4-8-3-c-p_ga: Úsáidtear _méid_ seachas _méad_ i gcásanna áirithe, e.g., _dá mhéad airgead a bhí aici_ ach _a mhéid is indéanta le réasún_; _a mhéid is gá_.
-:4-8-3-c-p_en: [TODO] 
+:4-8-3-c-p_en: _méid_ is used instead of _méid_ in certain cases, e.g., _dá mhéad airgead a bhí aici_ but _a mhéid is indéanta le réasún_; _a mhéid is gá_.
 
 :4-9-title_ga: An Aidiacht Shealbhach
 :4-9-title_en: The Possessive Adjective
 
 :4-9-p_ga: De bhrí go bhfeidhmíonn na haidiachtaí sealbhacha, _mo_, _do_, _a_ (firinscneach agus baininscneach uatha), _ár_, _bhur_ agus _a_ (iolra) mar fhorainmneacha sa ghinideach, déantar trácht orthu i gCaibidil 8.
-:4-9-p_en: [TODO] 
+:4-9-p_en: Because the possessive adjectives, _mo_, _do_, _a_ (masculine and feminine singular), _ár_, _bhur_ and _a_ (plural) function as pronouns in the genitive, they are discussed in Chapter 8.
 
 :4-10-title_ga: An Aidiacht Thaispeántach
 :4-10-title_en: The Deomonstrative Adjective
 
 :4-10-p_ga: Is iad _seo_, _sin_, _siúd_ agus _úd_ na haidiachtaí taispeántacha. (Cé gur forainmnigh thaispeántacha iad _seo_, _sin_ agus _siúd_, is minic a dhéantar tagairt dóibh mar aidiachtaí taispeántacha.)
-:4-10-p_en: [TODO] 
+:4-10-p_en: The demonstrative adjectives are _seo_, _sin_, _siúd_ and _úd_. (Although _seo_, _sin_ and _siúd_ are demonstrative pronouns, they are often referred to as demonstrative adjectives.)
 
 :4-11-title_ga: An Aidiacht Fhaisnéiseach
 :4-11-title_en: The Predicative Adjective
 
 :4-11-p_ga: Is aidiacht fhaisnéiseach aidiacht atá ag cáiliú an ainmní mar fhaisnéis nó mar chuid den fhaisnéis, e.g., _Nach deas é?_; _Tá an aimsir fuar_. Ní dhéantar an aidiacht fhaisnéiseach a infhilleadh ná ní chuirtear séimhiú uirthi.
-:4-11-p_en: [TODO] 
+:4-11-p_en: An predicative adjective is an adjective that qualifies the noun as predicate or as part of a predicate, e.g., _Nach deas é?_; _Tá an aimsir fuar_. The predicative adjective is not inflected or lenited.
 
 :4-12-title_ga: An Aidiacht Bhriathartha
 :4-12-title_en: The Verbal Adjective
 
 :4-12-1-p_ga: Feidhmíonn an aidiacht bhriathartha mar ghnáthaidiacht. Úsáidtear an aidiacht bhriathartha --
-:4-12-1-p_en: [TODO] 
+:4-12-1-p_en: The verbal adjective functions as a regular adjective. The verbal adjective is used --
 
 :4-12-1-a-p_ga: mar aidiacht cháilíochta, e.g., _Is bean phósta í_;
-:4-12-1-a-p_en: [TODO] 
+:4-12-1-a-p_en: as a qualifying adjective, e.g., _Is bean phósta í_;
 
 :4-12-1-b-p_ga: mar aidiacht fhaisnéiseach ar lorg na copaile, e.g., _Ní briste atá sé_;
-:4-12-1-b-p_en: [TODO] 
+:4-12-1-b-p_en: as a predicative adjective following the copula, e.g., _Ní briste atá sé_;
 
 :4-12-1-c-p_ga: mar aidiacht fhaisnéiseach ar lorg an bhriathair _bí_ ag léiriú staid an chuspóra tar éis an ghnímh, e.g., _Is í atá tugtha don obair_;
-:4-12-1-c-p_en: [TODO] 
+:4-12-1-c-p_en: as a predicative adjectve following the verb _bí_ indicating the state of the object after the action, e.g., _Is í atá tugtha don obair_;
 
 :4-12-1-d-p_ga: mar aidiacht fhaisnéiseach ar lorg an bhriathair _bí_ ag léiriú staid an ghníomhaí tar éis an ghnímh, e.g., _Táimid beirthe go maith leis an socrú sin_.
-:4-12-1-d-p_en: [TODO] `;export{a as default};
+:4-12-1-d-p_en: as a predicative adjective following the verb _bí_ indicating the state of the agent after the action, e.g., _Táimid beirthe go maith leis an socrú sin_.`;export{a as default};
