a7673e11b9f37af033df53b9dd9de5c22051947e
Zachary Bian <calculuswhiz@gmail.com>
2025-12-20 11:21:04 -0500

    Adjusted rootRef for compass images

---
+++ docs/assets/chapter3-DW32mebf.js
@@ -0,0 +1,1 @@
+import{c as e,j as t}from"./index-BG9hCbD8.js";import{C as o}from"./Chapter-C78xcvJ9.js";e.createRoot(document.getElementById("root")).render(t.jsx(o,{number:3,language:"en",onContentReady:()=>{}}));
